e8466328ceb03fd312b25f726b2581d4379735f8
satish-rajnale <satishrajnale98@gmail.com>
2021-04-13 18:43:05 +0530

    server location modified

--- config/index.js
+++ config/index.js
@@ -1,3 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
+
 export const server = 'http://localhost:3000'
+
+// export const server = dev ? 'http://localhost:3000' : 'https://yourwebsite.com'
